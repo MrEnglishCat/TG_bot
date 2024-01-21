e94371458da684bd5b8536695814227c9096d630
Memris <mr.englishcat@gmail.com>
2024-01-21 20:55:57 +0300

    guess_the_number.py update.add.start

--- guess_the_number.py
+++ guess_the_number.py
@@ -3,26 +3,31 @@ from aiogram import Bot, Dispatcher
 from aiogram.filters import Command
 from aiogram.types import Message, ContentType
 from aiogram import F
+from random import randint
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
 class Gamer:
-    def __init__(self, name:str, score:int, isgame:bool):
+    def __init__(self, name:str, score:int,  isgame:bool):
         self.name = name
         self.score = score
+        self.bot_number = randint(101)
+        self.user_number = None
         self.isgame = isgame
 
 
 @dp.message(Command(commands=['start']))
 async def start(message:Message):
-    await message.reply(text="Let's starts!!!")
+    gamer = Gamer(message.from_user.username, 0, True)
+    await message.reply(text=f"Let's starts!!!\nYour gamedata:\n   Name: {gamer.name}\n   Score: {gamer.score}.")
 
 
-# @dp.message()
-# async def start(message:Message):
-#     await message.send_copy(chat_id=message.chat.id)
-#
+@dp.message()
+async def start(message:Message):
+    print(gamer.name, gamer.score, gamer.isgame)
+    await message.send_copy(chat_id=message.chat.id)
+
 
 if __name__ == '__main__':
     dp.run_polling(bot)
