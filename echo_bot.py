edef2b326ecd8fc3c7fb41481682e6715c93cecc
Memris <mr.englishcat@gmail.com>
2024-01-21 20:22:33 +0300

    second commit

--- echo_bot.py
+++ echo_bot.py
@@ -31,33 +31,33 @@ async def process_help_command(message: Message):
 @dp.message()
 async def send_echo(message: Message):
     try:
-        pprint(message.model_dump_json(indent=3))
+        pprint(message)
         await message.send_copy(chat_id=message.chat.id)
     except TypeError:
         await message.reply(
             text='Данный тип апдейтов не поддерживается '
                  'методом send_copy'
         )
-
-
-# Этот хэндлер будет срабатывать на отправку боту фото
-@dp.message(F.content_type == ContentType.PHOTO)
-async def send_photo_echo(message: Message):
-    pprint(message)
-    await message.reply_photo(message.photo[0].file_id)
-
-@dp.message(F.sticker)
-async def send_animation(message: Message):
-    pprint(message)
-    await message.reply_sticker(message.sticker.file_id)
-    # await message.answer_animation(message.sticker.file_id)
-
-# Этот хэндлер будет срабатывать на любые ваши текстовые сообщения,
-# кроме команд "/start" и "/help"
-@dp.message()
-async def send_echo(message: Message):
-    await message.reply(text=message.text)
-
+#
+#
+# # Этот хэндлер будет срабатывать на отправку боту фото
+# @dp.message(F.content_type == ContentType.PHOTO)
+# async def send_photo_echo(message: Message):
+#     pprint(message)
+#     await message.reply_photo(message.photo[0].file_id)
+#
+# @dp.message(F.sticker)
+# async def send_animation(message: Message):
+#     pprint(message)
+#     await message.reply_sticker(message.sticker.file_id)
+#     # await message.answer_animation(message.sticker.file_id)
+#
+# # Этот хэндлер будет срабатывать на любые ваши текстовые сообщения,
+# # кроме команд "/start" и "/help"
+# @dp.message()
+# async def send_echo(message: Message):
+#     await message.reply(text=message.text)
+#
 
 
 
