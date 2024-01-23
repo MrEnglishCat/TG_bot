6cc05b0b3356f3864d21b02ed11dfa71a2b040dd
Memris <mr.englishcat@gmail.com>
2024-01-23 09:22:05 +0300

    guess_the_number.py update.add.start

---
+++ test.py
@@ -0,0 +1,6 @@
+from aiogram.filters import ChatMemberUpdatedFilter, KICKED
+from aiogram.types import ChatMemberUpdated
+
+from aiogram import F
+
+F.from_user.id.in_({193905674, 173901673, 144941561})
